6b5d255d0894228b54bf3ed83482ec21293a93fb
dev-shubht <webdevshubh@gmail.com>
2025-08-20 14:09:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AddJob, Admin, AllJobs, DashboardLayout, DeleteJob, EditJob, Error, HomeLayout, Landing, Login, Profile, Register, Stats } from './pages'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+
+      {
+        path: 'Register',
+        element: <Register />,
+      },
+      {
+        path: 'Login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
+            element: <AddJob />
+          },
+          {
+            path: "stats",
+            element: <Stats />
+          },
+          {
+            path: "all-jobs",
+            element: <AllJobs />
+          },
+          {
+            path: "profile",
+            element: <Profile />
+          },
+          {
+            path: "admin",
+            element: <Admin />
+          },
+        ]
+      },
+
+    ],
+  },
+
+
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App
---
+++ src/pages/Register.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { FormRow, Logo } from '../components'
+
+const Register = () => {
+    return (
+        <Wrapper>
+            <form action="" className='form'>
+                <Logo />
+                <h4>Register</h4>
+                <FormRow type='text' name='name'  defaultValue='shubh'/>
+                <FormRow type='text' name='lastName' labelText='last name'  defaultValue='shubh'/>
+                <FormRow type='text' name='location'  defaultValue='delhi'/>
+                <FormRow type='email' name='email'  defaultValue='shubh@gmail.com'/>
+                <FormRow type='password' name='password'  defaultValue='shubh123'/>
+                <button type='submit' className="btn btn-block">
+                    submit
+                </button>
+                <p>
+                    Already a member?
+                    <Link to='/login' className='member-btn'>
+                        Login
+                    </Link>
+                </p>
+            </form>
+        </Wrapper>
+    )
+}
+
+export default Register
